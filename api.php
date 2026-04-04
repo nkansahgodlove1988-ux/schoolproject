55a20d2e37b317bc6afb185e75af914ba91fb83d
nkansahgodlove1988-ux <nkansahgodlove1988@gmail.com>
2026-04-04 21:50:38 +0000

    Enable detailed error reporting at the top of api.php

--- api.php
+++ api.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
